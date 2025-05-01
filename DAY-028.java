404c8f9bec12c964f0bfd74260a66444b82edd83
Bhandavi19 <neelabhandavi@gmail.com>
2025-05-01 23:19:55 +0530

    DAY-028.java

---
+++ DAY-028.java
@@ -0,0 +1,19 @@
+// Number of occurrence
+// Problem Statement : Given a sorted array, arr[] and a number target, you need to find the number of occurrences of target in arr[]. 
+// Examples :
+// Input: arr[] = [1, 1, 2, 2, 2, 2, 3], target = 2
+// Output: 4
+// Explanation: target = 2 occurs 4 times in the given array so the output is 4.
+
+class Solution {
+    int countFreq(int[] arr, int target) {
+        int count=0;
+        int n=arr.length;
+        for(int i=0;i<n;i++){
+            if(arr[i] == target){
+                count++;
+            }
+        }
+        return count;
+    }
+}
